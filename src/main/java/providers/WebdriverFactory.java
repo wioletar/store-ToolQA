9e78bbccebae7d78918c47b599af752de80eadd7
Wioleta Rudzka <wrudzka@pl.sii.eu>
2018-11-19 15:35:26 +0100

    grid changes

--- src/main/java/providers/WebdriverFactory.java
+++ src/main/java/providers/WebdriverFactory.java
@@ -37,7 +37,7 @@ public class WebdriverFactory {
 
     private WebDriver getGrid() throws MalformedURLException {
         DesiredCapabilities cap = DesiredCapabilities.chrome();
-        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.56.1:4445/wd/hub"), cap);
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.56.1:4444/wd/hub"), cap);
         return driver;
     }
 
